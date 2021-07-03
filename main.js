7d9179059169e0c8d33f53d46fd09ee8e45fd2dd
GabrielDSRodrigues <gabrielrodrigues.sp.br@gmail.com>
2021-07-03 11:06:54 -0300

    Correções

--- main.js
+++ main.js
@@ -21,11 +21,13 @@ let optionsVal = [
   [1024, 600], [1024, 768], [1280, 768], [1280, 950], [1366, 768] //Notebooks
 ];
 let options = id("options");
+id("janela").style.width = optionsVal[0][0] + "px";
+id("janela").style.height = optionsVal[0][1] + "px";
 options.addEventListener("change", () => {
   camposLH[0].value = optionsVal[parseInt(options.value)][0];
   camposLH[1].value = optionsVal[parseInt(options.value)][1];
   if (url.value != "") {
     id("janela").style.width = optionsVal[parseInt(options.value)][0] + "px";
-    id("janela").style.heigth = optionsVal[parseInt(options.value)][1] + "px";
+    id("janela").style.height = optionsVal[parseInt(options.value)][1] + "px";
   }
 });
