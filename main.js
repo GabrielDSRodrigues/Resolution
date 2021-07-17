2f089ccc7a9c313a337d200c364a2a31e921b157
GabrielDSRodrigues <gabrielrodrigues.sp.br@gmail.com>
2021-07-16 23:45:47 -0300

    Adição de resoluções mobile

--- main.js
+++ main.js
@@ -19,25 +19,45 @@ btnSize.addEventListener("click", () => {
   }
 })
 
-let optionsVal = [
+let optionsVal = {
+  desktop: [
   [1024, 600], [1024, 768], [1280, 768], [1280, 950], [1366, 768], // Notebooks
   [1600, 900], [1920, 1080], [2160, 1440], [2560, 1440], [3200, 1800], [3440, 1440], [3840, 2160], [5120, 2880], [7680, 4320] // Monitores e Televisores    
-];
-
-let options = id("options");
-
-for (let i = 0; i < optionsVal.length; i++) {
-  options.insertAdjacentHTML("beforeend", `<option value="${i}">${optionsVal[i][0]} x ${optionsVal[i][1]}</option>`);
+],
+  mobile: [
+  [320, 480], [320, 533], [360, 640], [375, 667], [384, 640], [400, 640], [414, 736], [424, 753], [480, 854], [540, 960], [720, 1280]
+]
 }
 
-janela.style.width = optionsVal[0][0] + "px";
-janela.style.height = optionsVal[0][1] + "px";
-document.querySelector("header").style.width = optionsVal[0][0] + "px";
+let options = {
+  desktop: id("desktop"),
+  mobile: id("mobile")
+}
 
-options.addEventListener("change", () => {
-  document.querySelector("header").style.width = optionsVal[parseInt(options.value)][0] + "px";
-  camposLH[0].value = optionsVal[parseInt(options.value)][0];
-  camposLH[1].value = optionsVal[parseInt(options.value)][1];
-  janela.style.width = optionsVal[parseInt(options.value)][0] + "px";
-  janela.style.height = optionsVal[parseInt(options.value)][1] + "px";
+for (let i = 0; i < optionsVal.desktop.length; i++) {
+  options.desktop.insertAdjacentHTML("beforeend", `<option value="${i}">${optionsVal.desktop[i][0]} x ${optionsVal.desktop[i][1]}</option>`);
+}
+
+for (let i = 0; i < optionsVal.mobile.length; i++) {
+  options.mobile.insertAdjacentHTML("beforeend", `<option value="${i}">${optionsVal.mobile[i][0]} x ${optionsVal.mobile[i][1]}</option>`);
+}
+
+janela.style.width = optionsVal.desktop[0][0] + "px";
+janela.style.height = optionsVal.desktop[0][1] + "px";
+document.querySelector("header").style.width = optionsVal.desktop[0][0] + "px";
+
+options.desktop.addEventListener("change", () => {
+  document.querySelector("header").style.width = optionsVal.desktop[parseInt(options.desktop.value)][0] + "px";
+  camposLH[0].value = optionsVal.desktop[parseInt(options.desktop.value)][0];
+  camposLH[1].value = optionsVal.desktop[parseInt(options.desktop.value)][1];
+  janela.style.width = optionsVal.desktop[parseInt(options.desktop.value)][0] + "px";
+  janela.style.height = optionsVal.desktop[parseInt(options.desktop.value)][1] + "px";
+});
+
+options.mobile.addEventListener("change", () => {
+  document.querySelector("header").style.width = "100%";
+  camposLH[0].value = optionsVal.mobile[parseInt(options.mobile.value)][0];
+  camposLH[1].value = optionsVal.mobile[parseInt(options.mobile.value)][1];
+  janela.style.width = optionsVal.mobile[parseInt(options.mobile.value)][0] + "px";
+  janela.style.height = optionsVal.mobile[parseInt(options.mobile.value)][1] + "px";
 });
